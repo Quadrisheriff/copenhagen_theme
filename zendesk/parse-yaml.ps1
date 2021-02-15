a7bf090048649fa3dbf602a9ba814677930b1641
Quadri Sheriff <quadrisheriff0@gmail.com>
2021-02-15 10:01:00 +0100

    zendesk

---
+++ zendesk/parse-yaml.ps1
@@ -0,0 +1,69 @@
+
+#aim: script that is used as entry-point for creating a KB article for Snowplow Analytics and offers to push all new/modified docs to Zendesk
+
+$intro = @"      
+  ~~~~~~~~~~~~~~~~~~~
+  Docs 
+  ~~~~~~~~~~~~~~~~~~~
+  
+  CREATE NEW ONE OR  
+  ... PUSH THEM ALL TO ZD    
+  
+"@
+
+function convertTo-fileName ($docTitle) {
+    #aim: convert "Document Title" into "Document-Title.md"
+    $fileName = ("$docTitle" -replace "\s", "-") -replace ".+", "$&.md"
+    return $fileName
+}
+function update-configFile($docTitle, $fileName) {
+    #aim: create a new node in the config.yaml and populate it with doc title and file path
+    $conf = "/Users/salius/copenhagen_theme/zendesk\config.yaml"
+    $existingDocs = gc $conf | ConvertFrom-Yaml -Ordered
+    $newDoc = @([ordered]@{
+            id      = $null; 
+            title   = "$docTitle"; 
+            file    = "../docs/$fileName"; 
+            hash    = $null 
+            webLink = $null
+        })
+    
+    $articlesHelper = $existingDocs.sections.articles
+    $articlesHelper += $newDoc
+    
+    $existingDocs.sections[0].remove('articles')
+    $existingDocs.sections[0].add('articles', $articlesHelper)
+    
+    ConvertTo-Yaml $existingDocs -OutFile $conf -Force
+    #Invoke-Item $conf
+}
+
+function publish-toZendesk {
+    #aim: run the publish.js script that publishes new and updated docs to zendesk
+    $scriptFolder = "/Users/salius/copenhagen_theme/zendesk"
+    $scriptFile = "publish.js"
+    cd..
+    git add . 
+    git commit -m "zendesk updates" 
+    git push
+    Start-Process node $scriptFile -WorkingDirectory $scriptFolder 
+    Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -foregroundcolor Darkcyan
+    Write-Host "new documentation pushed to zendesk" -foregroundcolor Darkcyan
+    Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -foregroundcolor Darkcyan
+}
+
+Write-Host $intro
+$userSelection = Read-Host "ENTER TITLE OR WRITE 'PUSH' TO SEND TO ZENDESK"
+switch ($userSelection) {
+    "push" { 
+        publish-toZendesk
+    }
+    default { 
+        $docTitle = $userSelection
+        $fileName = convertTo-fileName -docTitle $docTitle
+        create-NewDoc -filename $fileName
+        update-configFile -docTitle $docTitle -fileName $fileName
+    }
+    
+}
+Pause
